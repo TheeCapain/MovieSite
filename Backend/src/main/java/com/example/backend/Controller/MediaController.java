7b29fcd572aae7c54cc79741f5f1e25751524c41
August <Augusthauerslev@outlook.com>
2022-06-29 09:16:36 +0200

    C+R virker nu fejl i findmedia

--- Backend/src/main/java/com/example/backend/Controller/MediaController.java
+++ Backend/src/main/java/com/example/backend/Controller/MediaController.java
@@ -3,15 +3,14 @@ package com.example.backend.Controller;
 import com.example.backend.Model.Media.Master.Media;
 import com.example.backend.Service.MediaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @CrossOrigin
-@RequestMapping("/media")
+@RequestMapping("media")
 public class MediaController {
 
     @Autowired
@@ -22,8 +21,8 @@ public class MediaController {
         mediaService.saveMedia(media);
     }
 
-    @GetMapping("/find/{id}")
-    public Optional<Media> findMedia(@PathVariable int id) {
+    @GetMapping("/{id}")
+    public Optional<Media> findMediaById(@PathVariable int id) {
         System.out.println("Test");
         return mediaService.findMedia(id);
     }
--- Backend/src/main/java/com/example/backend/config/initData.java
+++ Backend/src/main/java/com/example/backend/config/initData.java
@@ -2,20 +2,27 @@ package com.example.backend.config;
 
 import com.example.backend.Model.Media.Master.Media;
 import com.example.backend.Model.Media.Movie;
-import com.example.backend.Repository.MediaRepository;
+import com.example.backend.Service.MediaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class initData implements CommandLineRunner {
+
     @Autowired
-    MediaRepository mediaRepository;
+    MediaService mediaService;
 
     @Override
     public void run(String... args) throws Exception {
         Media media1 = new Movie();
         media1.setTitle("Hello");
-        mediaRepository.save(media1);
+        mediaService.saveMedia(media1);
+        Media media2 = new Movie();
+        media1.setTitle("Hello");
+        mediaService.saveMedia(media2);
+        Media media3 = new Movie();
+        media1.setTitle("Hello");
+        mediaService.saveMedia(media3);
     }
 }
